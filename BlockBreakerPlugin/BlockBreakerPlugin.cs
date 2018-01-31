e990af401c91aa7e0b035861eabf9af3b52b3437
Pedro Henriques <pedrohn13@gmail.com>
2018-01-31 15:36:26 -0300

    Kick off

---
+++ BlockBreakerPlugin/BlockBreakerPlugin.cs
@@ -0,0 +1,34 @@
+﻿using Photon.Hive.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockBreakerPlugin
+{
+    public class BlockBreakerPlugin : PluginBase
+    {
+        public BlockBreakerPlugin()
+        {
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "BlockBreakerPlugin";
+            }
+        }
+
+        public override void OnCreateGame(ICreateGameCallInfo info)
+        {
+            base.OnCreateGame(info);
+            Console.WriteLine("SALA CRIADA!!!");
+        }
+
+        public override void OnRaiseEvent(IRaiseEventCallInfo info)
+        {
+            base.OnRaiseEvent(info);
+            
+        }
+    }
+}
---
+++ BlockBreakerPlugin/PluginFactory.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Hive.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockBreakerPlugin
+{
+    class PluginFactory : IPluginFactory
+    {
+        public IGamePlugin Create(
+              IPluginHost gameHost,
+              string pluginName, // name of plugin requested by client
+              Dictionary<string, string> config, // plugin settings
+              out string errorMsg)
+        {
+            var plugin = new BlockBreakerPlugin();
+            if (plugin.SetupInstance(gameHost, config, out errorMsg))
+            {
+                return plugin;
+            }
+            return null;
+        }
+    }
+}
